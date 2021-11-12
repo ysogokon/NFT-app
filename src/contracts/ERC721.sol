5c835e75559d9af12dfd51a1ecb1335207854fa3
Yuriy Sogokon <ysogokon@honovi.com>
2021-11-12 16:33:05 -0500

    Wokring on section 12
    Signed-off-by: Yuriy Sogokon <ysogokon@honovi.com>

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -12,5 +12,27 @@ pragma solidity ^0.8.0;
       where it is being minted to, the ID
 */
 contract ERC721 {
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId
+    );
 
+    mapping(uint256 => address) private _tokenOwnder;
+    mapping(address => uint256) private _ownedTokensCount;
+
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        address owner = _tokenOwnder[tokenId];
+        return owner != address(0);
+    }
+
+    function _mint(address to, uint256 tokenId) internal {
+        require(to != address(0), "ERC721: minting to 0 address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _tokenOwnder[tokenId] = to;
+        _ownedTokensCount[to] += 1;
+
+        emit Transfer(address(0), to, tokenId);
+    }
 }
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -4,5 +4,23 @@ pragma solidity ^0.8.0;
 import "./ERC721Connector.sol";
 
 contract KryptoBird is ERC721Connector {
+    string[] public kryptoBirdz;
+    mapping(string => bool) _kryptoBirdExists;
+
+    function mint(string memory _kryptoBird) public {
+        require(
+            !_kryptoBirdExists[_kryptoBird],
+            "Error: kryptobird already exists"
+        );
+        // this is deprecated after ver 6.
+        //uint256 _id = KryptoBirdz.push(_kryptoBird);
+
+        kryptoBirdz.push(_kryptoBird);
+        uint256 _id = kryptoBirdz.length - 1;
+
+        _mint(msg.sender, _id);
+        _kryptoBirdExists[_kryptoBird] = true;
+    }
+
     constructor() ERC721Connector("KryptoBird", "KBIRDZ") {}
 }
